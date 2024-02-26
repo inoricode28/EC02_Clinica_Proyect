b55598b946a8ab2dfa64162d3a6082c4c9366a65
kotlin/semana5 <walter.store20@gmail.com>
2024-02-26 13:10:02 -0500

    se declaro la variable binding

--- Clinica_Proyect/app/src/main/java/pe/edu/idat/clinica_proyect/MainActivity.kt
+++ Clinica_Proyect/app/src/main/java/pe/edu/idat/clinica_proyect/MainActivity.kt
@@ -2,10 +2,13 @@ package pe.edu.idat.clinica_proyect
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import pe.edu.idat.clinica_proyect.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
