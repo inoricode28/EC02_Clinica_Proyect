116b317f7b79b150b929aae3da97e56aeaddcbae
kotlin/semana5 <walter.store20@gmail.com>
2024-02-26 13:18:16 -0500

    se implemento el evento onclick

--- Clinica_Proyect/app/src/main/java/pe/edu/idat/clinica_proyect/MainActivity.kt
+++ Clinica_Proyect/app/src/main/java/pe/edu/idat/clinica_proyect/MainActivity.kt
@@ -2,13 +2,18 @@ package pe.edu.idat.clinica_proyect
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import pe.edu.idat.clinica_proyect.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+    override fun onClick(v: View?) {
+        TODO("Not yet implemented")
     }
 }
