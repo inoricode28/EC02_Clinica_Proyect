9fb398ab248cdf1c47a83e1530474ebc2e2ee2e7
LuisT4s4yc0 <luistasaycoti@gmail.com>
2024-02-26 17:48:00 -0500

    Final Formulario

--- Clinica_Proyect/app/src/main/java/pe/edu/idat/clinica_proyect/FormActivity.kt
+++ Clinica_Proyect/app/src/main/java/pe/edu/idat/clinica_proyect/FormActivity.kt
@@ -2,10 +2,15 @@ package pe.edu.idat.clinica_proyect
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import pe.edu.idat.clinica_proyect.databinding.ActivityFormularioBinding
 
 class FormActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityFormularioBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityFormularioBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_formulario)
+
     }
 }
