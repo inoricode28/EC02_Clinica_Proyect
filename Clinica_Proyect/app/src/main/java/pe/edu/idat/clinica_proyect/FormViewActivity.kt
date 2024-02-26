13085a246e95bf05ac653f452cbc31688a4989d1
LuisT4s4yc0 <luistasaycoti@gmail.com>
2024-02-26 17:24:34 -0500

    Diseño de Vista para mostrar los datos ingresados

---
+++ Clinica_Proyect/app/src/main/java/pe/edu/idat/clinica_proyect/FormViewActivity.kt
@@ -0,0 +1,11 @@
+package pe.edu.idat.clinica_proyect
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FormViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form_view)
+    }
+}
