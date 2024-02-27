c549f6d47ac58eaa49ea1c4ebc5b85aac1b54ef8
inoricode28 <inori237@gmail.com>
2024-02-27 02:03:32 -0500

    AdapterItemFinal

--- Clinica_Proyect/app/src/main/java/pe/edu/idat/clinica_proyect/AdapterItem.kt
+++ Clinica_Proyect/app/src/main/java/pe/edu/idat/clinica_proyect/AdapterItem.kt
@@ -1,20 +1,17 @@
 package pe.edu.idat.clinica_proyect
-
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import pe.edu.idat.clinica_proyect.databinding.ItemValorBinding
 
 class AdapterItem(private var lista: List<String>) : RecyclerView.Adapter<AdapterItem.ViewHolder>(){
     inner class ViewHolder(val binding: ItemValorBinding) : RecyclerView.ViewHolder(binding.root)
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+        val binding = ItemValorBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
     }
-
     override fun getItemCount() = lista.size
-
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.binding.tvitem.setText(lista.get(position))
     }
-
 }
