2f9b0a94aeb1fd9efc0544ef5e89c98f5b3e3957
inoricode28 <inori237@gmail.com>
2024-02-27 02:15:08 -0500

    ListaElementosFinal_Modificar_Libros

--- Clinica_Proyect/app/src/main/java/pe/edu/idat/clinica_proyect/ListbookActivity.kt
+++ Clinica_Proyect/app/src/main/java/pe/edu/idat/clinica_proyect/ListbookActivity.kt
@@ -2,10 +2,26 @@ package pe.edu.idat.clinica_proyect
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import pe.edu.idat.clinica_proyect.databinding.ActivityListbookBinding
 
 class ListbookActivity : AppCompatActivity() {
+    lateinit var binding: ActivityListbookBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_listbook)
+        binding = ActivityListbookBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.rvelementos.layoutManager = LinearLayoutManager(applicationContext)
+        binding.rvelementos.adapter = AdapterItem(listaElementos())
+    }
+
+    private fun listaElementos(): List<String>{
+        val lista = ArrayList<String>()
+        var i = 0
+        while(i < 20){
+            lista.add("Libro $i")
+            i++;
+        }
+        return lista
     }
 }
