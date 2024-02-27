8b438d68b5f8d5b535a49bc984b4d5ecef674874
inoricode28 <inori237@gmail.com>
2024-02-27 07:37:15 -0500

    Implementando_Menu_Inicio

--- Clinica_Proyect/app/src/main/java/pe/edu/idat/clinica_proyect/MainActivity.kt
+++ Clinica_Proyect/app/src/main/java/pe/edu/idat/clinica_proyect/MainActivity.kt
@@ -8,15 +8,24 @@ import pe.edu.idat.clinica_proyect.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.btnRegistro.setOnClickListener(this)
 
+        // Configurar el listener de clic para cada botón
+        binding.btnRegistro.setOnClickListener(this)
+        binding.btnformulario.setOnClickListener(this)
+        binding.btnlistado.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
-        startActivity(Intent(application, FormActivity::class.java))
+        // Determinar qué botón fue clickeado y lanzar la actividad correspondiente
+        when (v?.id) {
+            R.id.btn_registro -> startActivity(Intent(this, FormActivity::class.java))
+            R.id.btnformulario -> startActivity(Intent(this, FrmCovidActivity::class.java))
+            R.id.btnlistado -> startActivity(Intent(this, ListbookActivity::class.java))
+        }
     }
 }
