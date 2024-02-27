143ad8b779c3e1b654ad5ef5e2e5e2d6e73acbaf
DarkcodeDev25 <DarkcodeDev25>
2024-02-27 04:08:35 -0500

    frmcovid

--- Clinica_Proyect/.idea/sonarlint/issuestore/index.pb
+++ Clinica_Proyect/.idea/sonarlint/issuestore/index.pb
@@ -3,3 +3,5 @@ m
 =app/src/main/java/pe/edu/idat/clinica_proyect/MainActivity.kt,f\9\f9bba77ba8b0b7b0bbb2a3b2da62a692deaa9a9a
 D
 app/build.gradle.kts,d\2\d2a4383452aad84a18429a5844d31330a012a9e6
+q
+Aapp/src/main/java/pe/edu/idat/clinica_proyect/FrmCovidActivity.kt,2\7\27b2ff1e1f417cc0ea8b896e5841ca088fd5715a
---
+++ Clinica_Proyect/app/src/main/java/pe/edu/idat/clinica_proyect/FrmCovidActivity.kt
@@ -0,0 +1,11 @@
+package pe.edu.idat.clinica_proyect
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FrmCovidActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_frm_covid)
+    }
+}
