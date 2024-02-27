56bcc5068bd622d785fde8c4e0264000a61ba388
DarkcodeDev25 <DarkcodeDev25>
2024-02-27 05:02:16 -0500

    frmcovidview

--- Clinica_Proyect/app/src/main/java/pe/edu/idat/clinica_proyect/FrmCovidViewActivity.kt
+++ Clinica_Proyect/app/src/main/java/pe/edu/idat/clinica_proyect/FrmCovidViewActivity.kt
@@ -2,10 +2,13 @@ package pe.edu.idat.clinica_proyect
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import pe.edu.idat.clinica_proyect.databinding.ActivityFrmCovidBinding
 
 class FrmCovidViewActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityFrmCovidBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_frm_covid_view)
+        binding = ActivityFrmCovidBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
